8e6c336ea8033621f65646e1400c4d29642acf42
timrcm <tim@timrcm.com>
2023-08-02 15:09:44 -0600

    find random db entry by id

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+import random
 
 
 app = Flask(__name__)
@@ -25,13 +26,22 @@ class Cafe(db.Model):
     coffee_price = db.Column(db.String(250), nullable=True)
 
 
-with app.create_contect():
+with app.app_context():
     db.create_all()
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route('/random', methods=['GET'])
+def random_cafe():
+    rows = db.session.query(Cafe).count()
+    rand_cafe_id = random.randint(1, rows)
+    cafe = db.session.execute(db.select(Cafe).filter_by(id=rand_cafe_id)).scalar()
+    print(cafe.name)
+    return f'{cafe.name}'
     
 
 ## HTTP GET - Read Record
