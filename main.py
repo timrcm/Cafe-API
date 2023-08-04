8410eb6d909337f1d9aac414d94dbc1ce49cac87
timrcm <tim@timrcm.com>
2023-08-04 15:04:53 -0600

    return random item as json

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import random
-
+from typing import TYPE_CHECKING
 
 app = Flask(__name__)
 
@@ -9,6 +9,14 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 db = SQLAlchemy()
 db.init_app(app)
+
+# Fix SQLAlchemy linting
+if TYPE_CHECKING:
+    from flask_sqlalchemy.model import Model
+
+    BaseModel = db.make_declarative_base(Model)
+else:
+    BaseModel = db.Model
 
 
 ##Cafe TABLE Configuration
@@ -35,14 +43,31 @@ def home():
     return render_template("index.html")
 
 
+@app.route('/all', methods=['GET'])
+def all_cafes():
+    pass
+
+
 @app.route('/random', methods=['GET'])
 def random_cafe():
+    # Fetch a random entry by primary key / row ID
     rows = db.session.query(Cafe).count()
     rand_cafe_id = random.randint(1, rows)
     cafe = db.session.execute(db.select(Cafe).filter_by(id=rand_cafe_id)).scalar()
-    print(cafe.name)
-    return f'{cafe.name}'
-    
+    # Can this be serialized to json in a simpler way? .__dict__ does not work on the object
+    return jsonify(name=cafe.name,
+                   map_url=cafe.map_url,
+                   location=cafe.location,
+                   seats=cafe.seats,
+                   img_url=cafe.img_url,
+                   id=cafe.id,
+                   has_wifi=cafe.has_wifi,
+                   has_toilet=cafe.has_toilet,
+                   has_sockets=cafe.has_sockets,
+                   coffee_price=cafe.coffee_price,
+                   can_take_calls=cafe.can_take_calls
+                   )
+
 
 ## HTTP GET - Read Record
 
